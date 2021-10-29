2a3739a565b104d1d6d07e25420a6e4065f829e0
Ana Micu <Ana.Micu@softwire.com>
2021-10-29 15:11:07 +0300

    E1: Create list of items using get_items function

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -8,4 +9,6 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = get_items()
+
+    return render_template('index.html', items = items)
