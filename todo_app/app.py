95473020fa536897339534345eddd5e0f15bd30f
Ana Micu <Ana.Micu@softwire.com>
2022-01-14 21:41:26 +0200

    E3: Add main function

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,23 +4,28 @@ from todo_app.data.view_model import ViewModel
 
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
+    @app.route('/')
+    def index():
+        items = get_items()
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model = item_view_model)
 
-@app.route('/')
-def index():
-    items = get_items()
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model = item_view_model)
+    @app.route('/add', methods = ['POST'])
+    def add_new_item():
+        title = request.form['title']
+        add_item(title)
+        return redirect(url_for('index'))
 
-@app.route('/add', methods = ['POST'])
-def add_new_item():
-    title = request.form['title']
-    add_item(title)
-    return redirect(url_for('index'))
+    @app.route('/change-status/<id>', methods = ['POST'])
+    def move_item(id):
+        change_status(id)
+        return redirect(url_for('index'))
 
-@app.route('/change-status/<id>', methods = ['POST'])
-def move_item(id):
-    change_status(id)
-    return redirect(url_for('index'))
+    return app
+
+if __name__ == '__main__':
+    create_app()
