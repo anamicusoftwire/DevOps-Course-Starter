a510e44cfd7b068376f7b9b27741106364382f84
Ana Micu <Ana.Micu@softwire.com>
2022-01-14 17:36:24 +0200

    E3: Add test for Doing items

--- todo_app/tests/unit_test.py
+++ todo_app/tests/unit_test.py
@@ -6,10 +6,22 @@ def test_it_returns_to_do_items():
         Card(1, 'Item 1', 'To Do'),
         Card(2, 'Item 2', 'Doing'),
         Card(3, 'Item 3', 'Done'),
-        Card(4, 'Item 1', 'To Do'),
+        Card(4, 'Item 4', 'To Do'),
     ]
 
     view_model = ViewModel(items)
 
     assert len(view_model.to_do_items) == 2
+
+def test_it_returns_doing_items():
+    items = [
+        Card(1, 'Item 1', 'To Do'),
+        Card(2, 'Item 2', 'Doing'),
+        Card(3, 'Item 3', 'Done'),
+        Card(4, 'Item 4', 'Doing'),
+    ]
+
+    view_model = ViewModel(items)
+
+    assert len(view_model.doing_items) == 2
     
