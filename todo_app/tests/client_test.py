f63d93ec082acbb19436a06cc2bde48eb908c980
Ana Micu <Ana.Micu@softwire.com>
2022-08-04 17:50:06 +0300

    E10: Rewrite test

--- todo_app/tests/client_test.py
+++ todo_app/tests/client_test.py
@@ -16,7 +16,9 @@ def client():
             yield client
 
 def test_index_page(client):
-    db = pymongo.MongoClient(os.environ.get('MONGO_CONNECTION_STRING'))[os.environ.get('MONGO_DATABASE_NAME')]
+    print(client.list_database_names())
+    db_connection = pymongo.MongoClient(os.environ.get('MONGO_CONNECTION_STRING'))
+    db = db_connection[os.environ.get('MONGO_DATABASE_NAME')]
     items = db.items
     new_item_name = "Test new item name"
     new_item = {
@@ -24,6 +26,9 @@ def test_index_page(client):
         "status": "To Do"
     }
     items.insert_one(new_item)
-    response = client.get("/").data.decode('utf8')
-
-    assert new_item_name in response
+    
+    response = client.get("/")
+    assert response.status_code == 200
+    
+    data = response.data.decode('utf8')
+    assert new_item_name in data
