56df010895a659a0853529bae0c014c2504736e5
Ana Micu <Ana.Micu@softwire.com>
2022-10-28 15:41:02 +0300

    E13: Add loggly integration

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,28 @@
+import os
 from flask import Flask, render_template, redirect, request, url_for
 from todo_app.data.authorization import get_identity, get_user_details
 from todo_app.data.trello_items import TrelloItems
 from todo_app.data.user import User
 from todo_app.data.view_model import ViewModel
 from flask_login import LoginManager, login_required, login_user
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
 
 from todo_app.flask_config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    loggly_token = os.environ.get('LOGGLY_TOKEN')
+
+
+    if loggly_token is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{loggly_token}/tag/todo-app')
+        handler.setFormatter(
+            Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+        )
+        app.logger.addHandler(handler)
+
     trello_items = TrelloItems(app.logger)
 
     @app.route('/')
